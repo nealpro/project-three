aa27f4bd8551426fd4d2fa6ff1803dc54d6b018d
nealpro <146895084+nealpro@users.noreply.github.com>
2023-11-17 00:58:55 +0000

    Add references section to page

--- src/routes/references/+page.svelte
+++ src/routes/references/+page.svelte
@@ -1,1 +1,13 @@
+<script>
+	const references = [
+		'<p>Bruni-Bossio, V., & Willness, C. (2016). The “Kobayashi Maru” Meeting: High-Fidelity</p><p class="indent-5">Experiential Learning. Journal of Management Education, 40(5), 619–647.<br></p><p class="indent-5">https://doi.org/10.1177/1052562916644284</p>'
+	];
+</script>
+
 <h1 class="text-4xl font-bold text-center p-6">References</h1>
+
+{#each references as reference}
+	<div class="rounded p-6 bg-white w-4/5 mx-auto border border-black space-y-4">
+		{@html reference}
+	</div>
+{/each}
