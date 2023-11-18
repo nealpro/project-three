840c1eb2289340e6bcfec43943eaf887e51e2a2b
Safecar28 <nealnpatelthegreat@gmail.com>
2023-11-17 23:53:03 -0700

    Update dependencies and content in +page.svelte and references/+page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,12 @@
 <script>
-	const content = [
-		"The Kobayashi Maru is a fictional exercise in the <i>Star Trek</i> universe, first introduced in the film <i>Star Trek II: The Wrath of Khan</i> (1982) and gained its popularity from that movie; however, it has appeared several times in the <i>Star Trek</i> franchise since then. It's a training exercise for cadets at the <i> Starfleet Academy</i>, designed to be a no-win scenario. The purpose of the test is to evaluate a cadet's character and command capabilities when faced with an impossible situation.",
+	const content1 = [
+		"The Kobayashi Maru is a fictional exercise in the <i>Star Trek</i> universe, first introduced in the film <i>Star Trek II: The Wrath of Khan</i> (1982) and gained its popularity from that movie; however, it has appeared several times in the <i>Star Trek</i> franchise since then. It's a training exercise for cadets at the <i>Starfleet Academy</i>, designed to be a no-win scenario. The purpose of the test is to evaluate a cadet's character and command capabilities when faced with an impossible situation.",
 		"In the scenario, a cadet is placed in command of a starship and receives a distress signal from the Kobayashi Maru, a civilian vessel stranded in the “Klingon Neutral Zone”. Any attempt to rescue the ship violates the “Neutral Zone” treaty and invites Klingon hostility, but ignoring the distress call contradicts Starfleet's humanitarian mission. The situation is engineered to be unwinnable, ensuring the cadet's failure.",
+		'The Kobayashi Maru gained significant popularity and cultural significance because of its association with the character James T. Kirk. Kirk is famously known for being the only person to have ever "beaten" the test by surreptitiously reprogramming the simulation so that a rescue was possible, thus demonstrating his aversion to accepting defeat. This act showcased his character\'s ingenuity, resourcefulness, and refusal to accept no-win scenarios, traits that became central to his identity in the <i>Star Trek</i> franchise.'
+	];
+
+	const content2 = [
+		"Over the years, the term <i>Kobayashi Maru</i> has entered broader pop culture. It's often used metaphorically to describe any situation where all available options are undesirable or where success seems unattainable. This reflects its role in <i>Star Trek</i> as a symbol of character test under pressure and moral dilemma, resonating with audiences beyond the science fiction genre."
 	];
 </script>
 
@@ -9,7 +14,7 @@
 
 <h2 class="text-4xl font-semibold text-center p-4">What is Kobayashi Maru?</h2>
 
-{#each content as item}
+{#each content1 as item}
 	<div class="rounded p-6 bg-white w-4/5 mx-auto border border-black">
 		<p>
 			{@html item}
@@ -18,7 +23,16 @@
 	<br />
 {/each}
 
-<slot />
+<h2 class="text-4xl font-semibold text-center p-4">Kobayashi Maru in pop culture</h2>
+
+{#each content2 as item}
+	<div class="rounded p-6 bg-white w-4/5 mx-auto border border-black">
+		<p>
+			{@html item}
+		</p>
+	</div>
+	<br />
+{/each}
 
 <style lang="postcss">
 	:global(html) {
--- src/routes/references/+page.svelte
+++ src/routes/references/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	const references = [
-		'<p>Bruni-Bossio, V., & Willness, C. (2016). The “Kobayashi Maru” Meeting: High-Fidelity</p><p class="indent-5">Experiential Learning. Journal of Management Education, 40(5), 619–647.<br></p><p class="indent-5">https://doi.org/10.1177/1052562916644284</p>'
+		'<p>Bruni-Bossio, V., & Willness, C. (2016). The “Kobayashi Maru” Meeting: High-Fidelity</p><p class="indent-5">Experiential Learning. Journal of Management Education, 40(5), 619–647.<br></p><p class="indent-5 text-blue-700 hover:underline"><a href="https://doi.org/10.1177/1052562916644284">https://doi.org/10.1177/1052562916644284</a></p>',
+		'<p>Meyer, N. (Director). (1982). Star Trek II: The Wrath of Khan [Film]. Paramount Pictures.</p>'
 	];
 </script>
 
@@ -14,6 +15,6 @@
 {/each}
 <div class="rounded p-1 mx-auto border bg-blue-100">
 	<p class="text-center font-mono">
-		Go back to <a href="/" class="text-blue-800"><u>Article</u></a>
+		Go back to <a href="/" class="text-blue-800 hover:underline">Article</a>
 	</p>
 </div>
